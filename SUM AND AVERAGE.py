ee29a5f9e4742ab4ecb910aea07d15aaf50efa43
CHARU AWASTHI <148922861+Charu19awasthi@users.noreply.github.com>
2024-05-08 01:51:51 -0700

    Update SUM AND AVERAGE.py

--- SUM AND AVERAGE.py
+++ SUM AND AVERAGE.py
@@ -1,15 +1,19 @@
-# n=input("enter the nos with space").split()
-# list_1=list(map(int,n))
-# c=0
-# total=len(list_1)
-# for i in list_1:
-#     c+=i
-# print(f"sum of all the elements is {c}")
-# print (f"average of list elements is {c/total}")
+# program for calculating sum and average of all element of list
+n=input("enter the nos with space").split()
+list_1=list(map(int,n))
+c=0
+total=len(list_1)
+for i in list_1:
+    c+=i
+print(f"sum of all the elements is {c}")
+print (f"average of list elements is {c/total}")
 
-# s=input("enter a string")
-# print(f"{s} and length of string is {len(s)}")
+#program for calculating length of string
+s=input("enter a string")
+print(f"{s} and length of string is {len(s)}")
 
+
+#program for reversing the number entered
 c= (input("enter atleast 2 digit no"))
 l=[]
 for i in c:
